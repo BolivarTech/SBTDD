b92ba858bbe4431a8b7971d73c6d79bc7032b491
Julian Bolivar <jbolivarg@gmail.com>
2026-04-19 16:16:32 -0400

    chore: scaffold scripts and reporters packages

---
+++ skills/sbtdd/scripts/__init__.py
@@ -0,0 +1,4 @@
+# Author: Julian Bolivar
+# Version: 1.0.0
+# Date: 2026-04-19
+"""sbtdd-workflow plugin scripts package."""
---
+++ skills/sbtdd/scripts/reporters/__init__.py
@@ -0,0 +1,4 @@
+# Author: Julian Bolivar
+# Version: 1.0.0
+# Date: 2026-04-19
+"""TDD-Guard reporters (stack-specific)."""
